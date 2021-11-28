34c86f4c04869fbd5d754201aba822ae7629e907
Sam Myers <github@sammye.rs>
2021-11-27 21:06:23 -0600

    Get and set tile groups
    Signed-off-by: Sam Myers <github@sammye.rs>

--- src/tile_group.rs
+++ src/tile_group.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use crate::error::Result;
 use crate::tile::Tile;
 
@@ -5,6 +6,16 @@ pub enum TileGroupLocation {
     Row(u8),
     Column(u8),
     House(u8, u8),
+}
+
+impl fmt::Display for TileGroupLocation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TileGroupLocation::Column(i) => write!(f, "Column({})", i),
+            TileGroupLocation::Row(i) => write!(f, "Row({})", i),
+            TileGroupLocation::House(i, j) => write!(f, "House({}, {})", i, j),
+        }
+    }
 }
 
 pub struct TileGroup {
@@ -19,5 +30,28 @@ impl TileGroup {
 
     fn validate(self) -> Result<Self> {
         Ok(self)
+    }
+
+    pub(crate) fn to_test_string(&self) -> String {
+        format!("{}{}{}{}{}{}{}{}{}",
+                self.tiles[0].to_test_string(),
+                self.tiles[1].to_test_string(),
+                self.tiles[2].to_test_string(),
+                self.tiles[3].to_test_string(),
+                self.tiles[4].to_test_string(),
+                self.tiles[5].to_test_string(),
+                self.tiles[6].to_test_string(),
+                self.tiles[7].to_test_string(),
+                self.tiles[8].to_test_string())
+    }
+}
+
+impl fmt::Display for TileGroup {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,
+               "{}: {}",
+               self.location,
+               self.to_test_string(),
+        )
     }
 }
